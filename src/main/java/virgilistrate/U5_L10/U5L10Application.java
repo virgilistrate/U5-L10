f924ad45f0e10accdfe3244c108d24f17acf2f1d
virgilistrate <virgilistrate24@gmail.com>
2026-02-13 09:27:05 +0100

    Initial commit

---
+++ src/main/java/virgilistrate/U5_L10/U5L10Application.java
@@ -0,0 +1,13 @@
+package virgilistrate.U5_L10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5L10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5L10Application.class, args);
+	}
+
+}
---
+++ src/test/java/virgilistrate/U5_L10/U5L10ApplicationTests.java
@@ -0,0 +1,13 @@
+package virgilistrate.U5_L10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5L10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
